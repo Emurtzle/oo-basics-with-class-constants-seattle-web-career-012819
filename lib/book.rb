010d80ae5bed6a3f70a72ffa26d470f9fbf9f452
Emily Wagner <emilywagner598@gmail.com>
2019-01-30 01:40:55 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,8 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_reader :title, :genre
 
-  GENRES - []
+  @@GENRES - []
 
   def initialize(title)
     @title = title
